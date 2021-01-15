f27061ab1f1a6fb82f65706a8c18b15a0578126a
srirag-vuppala <vuppala@calpoly.edu>
2021-01-14 22:22:38 -0800

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios'
 
 class App extends Component {
   state = {
@@ -8,7 +9,9 @@ class App extends Component {
   }
   removeCharacter = index => {
     const { characters } = this.state
-  
+
+    this.makeDeleteCall(characters[index])
+
     this.setState({ 
       characters: characters.filter((character, i) => {
         return i !== index
@@ -16,9 +19,55 @@ class App extends Component {
     })
   }
 
-  handleSubmit = character => {
-    this.setState({ characters: [...this.state.characters, character] })
+  makeDeleteCall(object){
+    return axios.delete('http://localhost:5000/users', {
+      data : {
+        'id': object.id,
+        'name': object.name,
+        'job': object.job
+    }})
+    .then(function(response) {
+      console.log(response);
+      return (response.status === 200);
+    })
+    .catch(function (error){
+      console.log(error);
+      return false;
+    });
   }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/users')
+    .then(res => {
+      const characters = res.data.users_list;
+      this.setState({ characters })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  makePostCall(character){
+    return axios.post('http://localhost:5000/users', character)
+    .then(function(response) {
+      console.log(response);
+      return response
+    })
+    .catch(function (error){
+      console.log(error);
+      return false;
+    });
+  }
+
+  handleSubmit = character => {
+    this.makePostCall(character).then( callResult => {
+      character.id = callResult.id
+      if (callResult.status === 201) {
+        this.setState({ characters: [...this.state.characters, character] });
+      }
+    });
+  }
+
   render() {
     const { characters } = this.state
     return(
